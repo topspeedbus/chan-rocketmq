5f51efe352e0899da998c50809b70396ffcd58b1
chenjie <243987701@qq.com>
2020-08-13 11:29:18 +0800

    Initial commit

---
+++ src/main/java/cn/chan/chanrocketmq/ChanRocketmqApplication.java
@@ -0,0 +1,13 @@
+package cn.chan.chanrocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanRocketmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChanRocketmqApplication.class, args);
+    }
+
+}
